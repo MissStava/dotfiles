13b365e8b5a5526893de346bd4d350beb23c736a
nullMethod <null.method@gmail.com>
2017-07-17 10:11:35 +0100

    Assume dotfiles will now live in ~/github

--- make-my-symlinks.sh
+++ make-my-symlinks.sh
@@ -1,14 +1,14 @@
 
 
 
-dir=~/dotfiles
-olddir=~/dotfiles_old
+dir=~/github/dotfiles
+olddir=~/github/dotfiles_old
 files="bashrc vimrc tmux.conf gitconfig vim scripts tmuxline.snapshot.conf"
 
 mkdir -p $olddir
 cd $dir
 
 for file in $files; do
-	mv ~/.$file ~/dotfiles_old/
+	mv ~/.$file $olddir/
 	ln -s $dir/$file ~/.$file
 done
