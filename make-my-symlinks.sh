8c2fba7ff73f63c56a886b7fabe557bb466fe0d5
Stephanie Hilton <null.method@gmail.com>
2019-05-17 14:40:31 +0100

    Refine installation

--- make-my-symlinks.sh
+++ make-my-symlinks.sh
@@ -2,16 +2,11 @@
 
 
 dir=~/github/dotfiles
-olddir=~/github/dotfiles_old
 files="bashrc vimrc tmux.conf gitconfig vim scripts tmuxline.snapshot.conf"
 
 mkdir -p $olddir
 cd $dir
 
 for file in $files; do
-	mv ~/.$file $olddir/
 	ln -s $dir/$file ~/.$file
 done
-
-mv ~/.config/Code/User/settings.json $olddir/
-ln -s $dir/settings.json ~/.config/Code/User/settings.json
